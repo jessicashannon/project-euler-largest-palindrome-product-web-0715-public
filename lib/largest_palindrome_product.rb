72ae4b98ea00da683107871270e4577e8a3f5b4c
jessicashannon <jessdahling@gmail.com>
2015-10-12 17:18:18 -0400

    done

--- lib/largest_palindrome_product.rb
+++ lib/largest_palindrome_product.rb
@@ -1,1 +1,21 @@
 # Implement your procedural solution here!
+
+# What is the largest palindrome product of two three-digit numbers?
+
+
+# Of the set of products of three-digit numbers
+# Check for palindrome
+
+def palindrome(number)
+  number.to_s == number.to_s.reverse
+end
+
+def largest_palindrome_product
+  array = []
+  (600..999).each{|num1|
+    (600..999).each{|num2|
+      if palindrome(num1*num2)
+        array << num1*num2
+      end}}
+  array.sort.last
+end
--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,19 @@
 # Implement your object-oriented solution here!
+
+class LargestPalindromeProduct
+
+  def palindrome(number)
+    number.to_s == number.to_s.reverse
+  end
+
+  def answer
+    array = []
+    (600..999).each{|num1|
+      (600..999).each{|num2|
+        if palindrome(num1*num2)
+          array << num1*num2
+        end}}
+    array.sort.last
+  end
+
+end
